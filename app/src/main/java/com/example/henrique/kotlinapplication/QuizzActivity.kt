5617b41ee45e91eaede6d88aacf94702c13b2cdc
Henrique Menezes <Henriquespmenezes@outlook.com>
2018-05-10 19:52:00 -0300

    Fun time

--- app/src/main/java/com/example/henrique/kotlinapplication/QuizzActivity.kt
+++ app/src/main/java/com/example/henrique/kotlinapplication/QuizzActivity.kt
@@ -26,8 +26,9 @@ class QuizzActivity : AppCompatActivity() {
             radioButtonA.setText("Resposta A. ?")
             radioButtonB.setText("Resposta B. ?")
             radioButtonC.setText("Resposta C. ?")
-            txtViewQuizz.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam iaculis tincidunt tortor id consequat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;\n Proin placerat rhoncus eleifend. Mauris purus tellus, fringilla ac risus ut, sodales posuere leo. Nullam faucibus lectus sit amet rutrum cursus. Aliquam mollis pharetra scelerisque. Aenean condimentum finibus nunc. Cras tempus nisi ut massa pretium, ut lacinia leo gravida. Nullam congue porttitor arcu, non aliquam tellus convallis ut.\n Ut imperdiet tellus at arcu vestibulum, et congue purus condimentum. Praesent ut tempor lectus. Duis rhoncus quis odio vel pulvinar. Donec vel purus felis. Aliquam.")
+            txtViewQuestion.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam iaculis tincidunt tortor id consequat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;\n Proin placerat rhoncus eleifend. Mauris purus tellus, fringilla ac risus ut, sodales posuere leo. Nullam faucibus lectus sit amet rutrum cursus. Aliquam mollis pharetra scelerisque. Aenean condimentum finibus nunc. Cras tempus nisi ut massa pretium, ut lacinia leo gravida. Nullam congue porttitor arcu, non aliquam tellus convallis ut.\n Ut imperdiet tellus at arcu vestibulum, et congue purus condimentum. Praesent ut tempor lectus. Duis rhoncus quis odio vel pulvinar. Donec vel purus felis. Aliquam.")
             btnAnswer.setText("CONFIRMAR")
+
         }
 
     }
