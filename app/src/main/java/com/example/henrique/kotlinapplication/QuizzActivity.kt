8196fcf54325be5a9d1c37561e182414774ded24
Henrique Menezes <Henriquespmenezes@outlook.com>
2018-05-11 17:33:49 -0300

    randomNumber

--- app/src/main/java/com/example/henrique/kotlinapplication/QuizzActivity.kt
+++ app/src/main/java/com/example/henrique/kotlinapplication/QuizzActivity.kt
@@ -2,6 +2,7 @@ package com.example.henrique.kotlinapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.widget.*
 import kotlin.concurrent.timer
 
@@ -18,9 +19,7 @@ class QuizzActivity : AppCompatActivity() {
         val radioButtonC = findViewById(R.id.radioButtonC) as RadioButton
         val txtViewQuestion = findViewById(R.id.textViewQuestion) as TextView
         val btnAnswer = findViewById(R.id.btnAnswer) as Button
-        var countTime = findViewById(R.id.chronometer) as Chronometer
-
-        countTime.start()
+        val countTime = findViewById(R.id.chronometer) as Chronometer
 
         btnAnswer.setOnClickListener {
 
@@ -31,5 +30,11 @@ class QuizzActivity : AppCompatActivity() {
             btnAnswer.setText("CONFIRMAR")
 
         }
+        fun random(a:Int, b:Int ): Int {
+
+            return a + b
+        }
+        }
+
     }
-}
+
